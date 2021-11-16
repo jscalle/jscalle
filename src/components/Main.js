2d92709dc25d39f435f1ea42d5de256923619005
Sebas Calle <l.oaiza.s@hotmail.com>
2021-11-16 18:49:56 -0500

    Delete package-lock.json

--- src/components/Main.js
+++ src/components/Main.js
@@ -6,14 +6,16 @@ function App() {
   return (
     <div className="styleApp">
       <header className="styleApp-header">
-        <img src={logo} className="styleApp-logo" alt="logo" />
-        <div className="intro">
-          <p> ¡Hola Mundo! soy Sebastian Calle <span>(jscalle)</span> este es un espacio para que conozcas un poco sobre mi y algunos de mis proyectos.</p>
+        <div className="aux" >
+          <img src={logo} className="styleApp-logo" alt="logo" />
+          <div className="intro">
+            <p> <span>¡Hola Mundo!</span> <br/><br/> Soy Sebastian Calle <span>@jscalle</span> este es un espacio para que conozcas un poco sobre mi y algunos de mis proyectos.</p>
+          </div>
+          <ul className="list">
+            <li><Link className="noLink" to="/misionnft">Misión NFT LATAM</Link></li>
+            <li><Link className="noLink" to="/cv">CV Personal</Link></li>
+          </ul>
         </div>
-        <ul className="list">
-          <li><Link className="styleApp-link" to="/misionnft">Misión NFT LATAM</Link></li>
-          <li><Link className="styleApp-link" to="/cv">CV Personal</Link></li>
-        </ul>
       </header>
     </div>
   );
